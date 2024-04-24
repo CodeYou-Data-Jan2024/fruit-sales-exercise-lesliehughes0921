fd3783b94efd77d0dd3666145c6c8ed8393f7379
Leslie Hughes <leslie.hughes0921@gmail.com>
2024-04-24 07:39:34 -0400

    added import pandas

--- fruitsales.py
+++ fruitsales.py
@@ -1,4 +1,6 @@
 # add your code hereimport pandas as pd
+import pandas as pd
+
 df_fruit_sales = pd.DataFrame({"Apples": [35, 41] , "Bananas": [21, 34]}, index = ["2017 Sales", "2018 Sales"])
 df_fruit_sales
 df_fruit_sales.to_csv("fruit.csv")
